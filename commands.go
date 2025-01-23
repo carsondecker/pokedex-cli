f43b665e0e56acc1ea5e47d5302feddedc33aaec
carsondecker <carsondecker1205@gmail.com>
2025-01-23 12:44:20 -0600

    Set up command handling

---
+++ commands.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+// Avoids initialization cycle with commands that reference the commands map
+func initCommands() {
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	for k, v := range commands {
+		fmt.Printf("\t%s: %s\n", k, v.description)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func main() {
+	initCommands()
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if scanner.Text() == "" {
+			fmt.Println("Please enter a command.")
+			continue
+		}
+		inputWords := cleanInput(scanner.Text())
+		if _, ok := commands[inputWords[0]]; !ok {
+			fmt.Println("Unknown command.")
+			continue
+		}
+		commands[inputWords[0]].callback()
+	}
+}
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.TrimSpace(strings.ToLower(text)))
